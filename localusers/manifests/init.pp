661df47bdc4744ef32d1cb325c771e04ee4c6506
tbui518 <tbui518@gmail.com>
2016-02-09 12:36:38 -0500

    no message

--- localusers/manifests/init.pp
+++ localusers/manifests/init.pp
@@ -3,7 +3,7 @@ class localusers {
                 ensure     =>  present,
                 shell      => '/bin/bash',
                 home       => '/home/admin',
-                gid        => 'wheel'
+                gid        => 'wheel',
                 managehome => 'true',
                 password   => '6$wBD0lWNR$P26crJqQu08uRrLj9dmZTHJH9YY8LO71GmhDAl68l2q5XZdWRbL9zn9jBpmH4rCFutHvLyiopRSnuhhomosj01',
              }
