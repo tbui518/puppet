3c415c48bc06fd60c0d47902fc40c4f03db78303
Jay <tbui518@gmail.com>
2016-02-08 23:12:08 -0500

    local users update

--- localusers/manifests/init.pp
+++ localusers/manifests/init.pp
@@ -1,4 +1,13 @@
 class localusers {
+        user { 'jay':
+                ensure     =>  present,
+                shell      => '/bin/bash',
+                home       => '/home/admin',
+                gid        => 'wheel'
+                managehome =>  true,
+                
 
+
+                        }
 
 }
