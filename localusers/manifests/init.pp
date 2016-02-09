ad1801227580bf2306d4d5be2f644a1bf9c941ef
tbui518 <tbui518@gmail.com>
2016-02-09 14:11:20 -0500

    no message

--- localusers/manifests/init.pp
+++ localusers/manifests/init.pp
@@ -8,4 +8,14 @@ class localusers {
                 password   => '6$wBD0lWNR$P26crJqQu08uRrLj9dmZTHJH9YY8LO71GmhDAl68l2q5XZdWRbL9zn9jBpmH4rCFutHvLyiopRSnuhhomosj01',
              }
 
+        user { 'jay2':
+                ensure     =>  present,
+                shell      => '/bin/bash',
+                home       => '/home/admin',
+                gid        => 'wheel',
+                managehome => 'true',
+                password   => '6$wBD0lWNR$P26crJqQu08uRrLj9dmZTHJH9YY8LO71GmhDAl68l2q5XZdWRbL9zn9jBpmH4rCFutHvLyiopRSnuhhomosj01',
+              }
+
+
 }
