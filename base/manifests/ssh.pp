c466b05e668fdb34e52dcf64b7389c87b3d4e26f
Jay <tbui518@gmail.com>
2016-02-13 18:19:13 -0500

    no message

---
+++ base/manifests/ssh.pp
@@ -0,0 +1,16 @@
+class base::ssh {
+          package { 'openssh':
+                         ensure => present,
+          }
+          file { '/etc/ssh/sshd_config':
+               ensure => file,
+               owner => 'root',
+               group => 'root,
+               require => Package ['openssh']
+               source => 'puppet:///modules/base/ssd_config',
+          }
+          service { 'sshd':
+               ensure => running,
+               enable => true,
+               require =>
+}
